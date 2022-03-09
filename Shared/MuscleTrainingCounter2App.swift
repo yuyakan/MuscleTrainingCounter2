065a73c8563adc848bab890ce77ad098c2bc284c
yuyakan <kanbe1365@icloud.com>
2022-03-09 22:52:44 +0900

    Initial Commit

---
+++ Shared/MuscleTrainingCounter2App.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleTrainingCounter2App.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/03/09.
+//
+
+import SwiftUI
+
+@main
+struct MuscleTrainingCounter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
